b4b2191f1f7eb589ae3c2aef64a583afc8dfcf03
Zak Habul <112983162+ptewie@users.noreply.github.com>
2022-09-26 12:38:41 -0700

    Update code.js

--- code.js
+++ code.js
@@ -14,67 +14,67 @@ var timeOut = 5000;
 
 //The settimeout function is the JavaScript built in function to delay the page rendering too fast.
 setTimeout(function(){
-    x.innerHTML = currTime
+    countDownStart.innerHTML = currTime
     currTime = currTime-5;
 }, timeOut);
 timeOut = timeOut + 5000;
 
 setTimeout(function(){
-    x.innerHTML = currTime //Copy and Pasting the setTimeout Function and subtracting currTime by 5 until it reaches 0
+    countDownStart.innerHTML = currTime //Copy and Pasting the setTimeout Function and subtracting currTime by 5 until it reaches 0
     currTime = currTime-5;
 }, timeOut);
 timeOut = timeOut + 5000;
 
 setTimeout(function(){
-    x.innerHTML = currTime 
+    countDownStart.innerHTML = currTime 
     currTime = currTime-5;
 }, timeOut);
 timeOut = timeOut + 5000;
 
 setTimeout(function(){
-    x.innerHTML = currTime 
+    countDownStart.innerHTML = currTime 
     currTime = currTime-5;
 }, timeOut);
 timeOut = timeOut + 5000;
 
 setTimeout(function(){
-    x.innerHTML = currTime 
+    countDownStart.innerHTML = currTime 
     currTime = currTime-5;
 }, timeOut);
 timeOut = timeOut + 5000;
 
 setTimeout(function(){
-    x.innerHTML = currTime 
+    countDownStart.innerHTML = currTime 
     currTime = currTime-5;
 }, timeOut);
 timeOut = timeOut + 5000;
 
 setTimeout(function(){
-    x.innerHTML = currTime 
+    countDownStart.innerHTML = currTime 
     currTime = currTime-5;
 }, timeOut);
 
 setTimeout(function(){
-    x.innerHTML = currTime 
-    currTime = currTime-5;
-}, timeOut);
-timeOut = timeOut + 5000;
-
-setTimeout(function(){
-    x.innerHTML = currTime 
+    countDownStart.innerHTML = currTime 
     currTime = currTime-5;
 }, timeOut);
 timeOut = timeOut + 5000;
 
 setTimeout(function(){
-    x.innerHTML = currTime 
+    countDownStart.innerHTML = currTime 
+    currTime = currTime-5;
+}, timeOut);
+timeOut = timeOut + 5000;
+
+setTimeout(function(){
+    countDownStart.innerHTML = currTime 
     currTime = currTime-5;
 }, timeOut);
 timeOut = timeOut + 5000;
 timeOut = timeOut + 5000;
 
 setTimeout(function(){   //Countdown over!!!!!
-    x.innerHTML = currTime 
+    countDownStart.innerHTML = currTime 
     alert("PACHOOOOOOO!!!!!!!! Blastoff!!!");
 }, timeOut);
 
