84c3350c50ee81ac4c382a5644e2416d0379c312
Zak Habul <112983162+ptewie@users.noreply.github.com>
2022-10-12 13:19:19 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,36 @@
+function getUserInput(nameElement){ //Getting the User's Name. 
+    //do while function 
+    do {
+        firstname = prompt("Hello! Please enter your first name (under 20 characters): ");  
+        lastname = prompt("Please enter your last name (under 20 characters):  ");
+
+        fullname = firstname + " " + lastname; //Defining the full name
+        //Checking to see if the name has more than 20 characters, alert shows if true
+        if (fullname.length > 20){
+            alert ("WARNING: " + fullname.length + " is too many characters in your name! Please resubmit.") 
+        }
+
+    } while (fullname.length > 20); //If name is greater then 15 characters, loop runs again
+
+
+    //Asking for the
+    
+    bnumber = 0 //defualt value of badge number
+
+    do {
+        bnumber = prompt("Please enter your badge number: (3 digits max)")
+        if (bnumber > 999){
+            alert("Error, more than 3 digits used, please retry.")
+        }
+    } while (bnumber > 999);
+    alert("Credentials confirmed! Logging you in!")
+
+    nameElement.innerHTML = fullname + " ID: " + bnumber;
+
+} 
+
+
+
 // DEFINING startButtonClick() function.
 
 function startButtonClick(){} //empty for now
@@ -32,9 +65,7 @@ function runTimer(countDownStart){
         setTimeout(function(){ //Basically a nested function
             currTime = currTime-5; // Creating Delay when loading up page
 
-            if (currTime > 25){  // changed < to >, creating logic error. 
-                // debugging alert should only appear when currTime is less than 25
-                alert("curr time variable" + currTime)
+            if (currTime < 25){  // changed < to >, creating logic error. 
                 countDownStart.innerHTML = "Warning Less than ½ way to launch, time left = " + currTime // Adding text before countdown
             }
             else   {
